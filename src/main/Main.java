5fc7a468f66c9fc4ab4e28cea609085fcff81b20
BlineraBerisha1 <blinera.berisha2@student.uni-pr.edu>
2026-04-12 13:10:43 +0200

    Krijimi i paketave dhe klasave te nevojshme per enkoderin/dekoderin base64

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import encoder.Base64Encoder;
+import decoder.Base64Decoder;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+    }
